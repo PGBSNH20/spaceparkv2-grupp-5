bd57b6dafda731ea4fe00b705b0bd386a4c0a73e
coachvredin <43240053+coachvredin@users.noreply.github.com>
2021-04-28 15:41:16 +0200

    Created the models.

---
+++ Source/SpaceParkAPI/Models/Receipt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceParkAPI.Models
+{
+    public class Receipt
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public int Price { get; set; }
+    }
+
+}
