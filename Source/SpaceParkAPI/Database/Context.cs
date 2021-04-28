6e3a69071d5ce05e1e8fdc30e643719fde238c7b
KevinKallin <kevin.kallin@hotmail.se>
2021-04-28 18:14:23 +0200

    Small fixes

--- Source/SpaceParkAPI/Database/Context.cs
+++ Source/SpaceParkAPI/Database/Context.cs
@@ -11,6 +11,12 @@ namespace SpaceParkAPI.Database
     {
         public DbSet<User> Users { get; set; }
 
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string connection = @"Server=Cesc\Sqlexpress;Database=SpaceparkAPI; User Id=cesc; Password=Fotboll8;";
