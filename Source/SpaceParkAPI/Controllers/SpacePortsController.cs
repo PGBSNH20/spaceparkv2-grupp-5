67a1e4d95b9384137be3818672d936e5afb390e9
coachvredin <43240053+coachvredin@users.noreply.github.com>
2021-05-04 10:38:56 +0200

    Added SpacePortsController.

---
+++ Source/SpaceParkAPI/Controllers/SpacePortsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SpaceParkAPI.Data;
+using SpaceParkAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceParkAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpacePortsController : ControllerBase
+    {
+        private SpaceDbContext _dbContext;
+
+        public SpacePortsController(SpaceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<SpacePort> GetAllSpacePorts()
+        {
+            return _dbContext.SpacePorts;
+        }
+
+        [HttpPost]
+        public IActionResult PostSpacePort([FromBody] SpacePort spacePort)
+        {
+            if(string.IsNullOrEmpty(spacePort.Name))
+            {
+                return BadRequest($"You need to enter a name for the spaceport.");
+            }
+
+            if (spacePort.ParkingSpots <= 0)
+            {
+                return BadRequest($"A spaceport needs a minimum of 1 parking spots.");
+            }
+
+            _dbContext.SpacePorts.Add(spacePort);
+            _dbContext.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+    }
+}
