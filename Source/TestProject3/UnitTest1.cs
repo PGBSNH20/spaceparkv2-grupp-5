5d8bad6c6e1f1e7ad5e5369d8798aa02cb471142
KevinKallin <kevin.kallin@hotmail.se>
2021-05-05 23:33:29 +0200

    Added a test method and added inmemory database

--- Source/TestProject3/UnitTest1.cs
+++ Source/TestProject3/UnitTest1.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Reflection.Metadata;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using SpaceParkAPI;
+using SpaceParkAPI.Controllers;
+using SpaceParkAPI.Data;
 using SpaceParkAPI.Models;
 using Xunit;
 
@@ -9,10 +12,26 @@ namespace TestProject3
 {
     public class UnitTest1
     {
-        [Fact]
-        public async Task PostUser_ValidInput_ExpectTrue()
+
+        private static DbContextOptions<SpaceDbContext> options = new DbContextOptionsBuilder<SpaceDbContext>()
+            .UseInMemoryDatabase(databaseName: "Test")
+            .Options;
+
+        private static SpaceDbContext _dbContext;
+        private static ParkingsController parkings = new ParkingsController(_dbContext);
+
+
+
+
+        public void SeedDataBase()
         {
-           
+            
+        }
+
+        [Fact]
+        public async Task PostUser_ValidUser_ExpectTrue()
+        {
+
             Swapi test = new Swapi();
 
             User user = new User()
@@ -30,7 +49,7 @@ namespace TestProject3
         }
 
         [Fact]
-        public async Task PostUser_InValidInput_ExpectFalse()
+        public async Task PostUser_InValidUser_ExpectFalse()
         {
             Swapi test = new Swapi();
 
@@ -47,5 +66,28 @@ namespace TestProject3
 
             Assert.False(result);
         }
+
+        [Fact]
+        public void Test1()
+        {
+            SpacePort spacePort = new SpacePort()
+            {
+                Id = 1,
+                Name = "Uddevalla Space Port",
+                ParkingSpots = 3,
+                UserName = "admin"
+            };
+
+            var result = false;
+
+            if (spacePort.UserName == "admin")
+            {
+                result = true;
+            }
+
+            Assert.True(result);
+        }
+
+        
     }
 }
